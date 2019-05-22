fb7a2737ac555c8a804ee8c3855b2310127d52d8
Midor2Mid <14520777@gm.uit.edu.vn>
2019-05-22 10:21:57 +0700

    php bin/console make:entity

---
+++ src/Entity/Profile.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ProfileRepository")
+ */
+class Profile
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Age;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Sex;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $DoB;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->Age;
+    }
+
+    public function setAge(int $Age): self
+    {
+        $this->Age = $Age;
+
+        return $this;
+    }
+
+    public function getSex(): ?string
+    {
+        return $this->Sex;
+    }
+
+    public function setSex(string $Sex): self
+    {
+        $this->Sex = $Sex;
+
+        return $this;
+    }
+
+    public function getDoB(): ?\DateTimeInterface
+    {
+        return $this->DoB;
+    }
+
+    public function setDoB(\DateTimeInterface $DoB): self
+    {
+        $this->DoB = $DoB;
+
+        return $this;
+    }
+}
