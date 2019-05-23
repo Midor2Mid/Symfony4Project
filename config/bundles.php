03ca12cc7482a85d7782c730ec462acf0cfac3d3
Midor2Mid <14520777@gm.uit.edu.vn>
2019-05-23 16:22:44 +0700

    composer req annotations

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
